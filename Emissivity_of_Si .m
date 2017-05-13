8fd7f3658eedd93ff94076b3ef43bca351e69359
caheredia <cristianheredia@gmail.com>
2017-05-13 15:23:00 -0700

    Emissivity from absorption coef.
    quickly calculate emissivity using Temperature, Reflectivity, ,and absorption coefficient

---
+++ Emissivity_of_Si .m
@@ -0,0 +1,20 @@
+clear, clc
+T = (300:1:800); %generates temperature range 
+ece = 0.88; %energy coupling efficiency
+P = 1.1;% incident power
+bc = 5.67*10^-12; %Stefan-Boltzmann constant 
+T0 = 300; % Room temp
+R = 0.3204; % Assumed Reflectivity 
+lambda = 1.55;
+
+%Absorption coefficient 
+alpha = 4.15*10^(-5)*lambda^(1.51)*T.^(2.95)*exp(-7000/T);
+
+a = T.^(4);
+b = T0^4;
+c = a - b ;
+d = a/c;
+
+em = (1-R)./(2*bc*(a-b));
+
+divc = T0./(T-T0);
